0143327b42d56d5505c538a4706acb2298ac5211
Ksenya Arysheva <ksendart@yandex.ru>
2026-05-21 11:21:19 +0700

    Iteration 1 - Add data-access/menu lib with httpResource
    - Generate libs/data-access/menu via @nx/angular:lib (strict)
    - Add MenuService: menu list resource + dishById factory using httpResource
    - Rename TS path alias to @restaurant/data-access-menu for consistency

---
+++ libs/data-access/menu/src/lib/menu.service.ts
@@ -0,0 +1,17 @@
+import { httpResource } from '@angular/common/http';
+import { Injectable, Signal } from '@angular/core';
+import { DishDto } from '@restaurant/shared-types';
+
+@Injectable({ providedIn: 'root' })
+export class MenuService {
+  readonly menu = httpResource<DishDto[]>(() => '/api/menu', {
+    defaultValue: [],
+  });
+
+  dishById(idSignal: Signal<string | null>) {
+    return httpResource<DishDto | undefined>(() => {
+      const id = idSignal();
+      return id ? `/api/menu/${id}` : undefined;
+    });
+  }
+}
